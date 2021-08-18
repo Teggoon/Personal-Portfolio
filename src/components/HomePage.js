2adcd8df576df3fe1a786ba54841b18ac80b158b
Teggoon <daniel.hc.li@gmail.com>
2021-08-17 22:48:36 -0700

    added positioning for the experience sections

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,22 +12,31 @@ const HomePage = () => {
 
             </div>
             
-            <div className="homepage-section-2">
+            <div className="experience-section-container experience-section-1">
             <h3>Solid foundation in HTML, CSS, and Javascript</h3>
             <p>
             As a developer who strives to be competent and versatile in web development, 
             I've spent much time learning the fundamentals of core web technology: HTML, CSS, 
-            Javascript, the DOM, HTTP, and 
+            Javascript, the DOM, and HTTP.
             </p>
             </div>
 
-            <div className="homepage-section-3">
+            <div className="experience-section-container experience-section-2">
             <h3>Full-stack awareness & exposure</h3>
+            <p>
+            During an internship at the University of Phoenix, I developed a web application for
+            their employees from scratch, coming into contact with many facets of a software project: 
+            frontend, backend, security, automated testing, and application monitoring.
+            </p>
             </div>
 
-            <div className="homepage-section-4">
+            <div className="experience-section-container experience-section-3">
             <h3>Agile methodology</h3>
-            <h3>Full-stack awareness & exposure</h3>
+            <p>
+            Having worked with an Agile scrum team during my internship, I have a solid understanding
+            of the Agile principles of iterative development, customer collaboration, and 
+            adaptiveness.
+            </p>
             </div>
             
         </div>
