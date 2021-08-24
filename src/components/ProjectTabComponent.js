822f6721a6abff0fb15e8001d992d542e193cb1e
Teggoon <daniel.hc.li@gmail.com>
2021-08-23 23:18:56 -0700

    added technologies used section to project tabs

--- src/components/ProjectTabComponent.js
+++ src/components/ProjectTabComponent.js
@@ -2,7 +2,19 @@ const ProjectTabComponent = ({title, description}) => {
     return ( 
         <div className="project-description-container">
                 <h3>{title}</h3>
-                <p>{description}</p>
+                <div className="columns-side-by-side">
+
+                    <p>{description}</p>
+                    <div className="technologies-used-container">
+                    <h5>Technologies used: </h5>
+                    <ul>
+                        <li>Frontend: React, CSS, HTML tables</li>
+                        <li>Backend: Node & Express, SQL</li>
+                        <li>Automated testing: Jest, Enzyme</li>
+                        <li>Security: LDAP authentication</li>
+                    </ul>
+                    </div>
+                </div>
         </div>
     );
 }
