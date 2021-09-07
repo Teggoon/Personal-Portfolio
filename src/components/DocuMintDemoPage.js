8dc56a5ade356ee851796d2fdc0bbec093e6781d
Teggoon <daniel.hc.li@gmail.com>
2021-09-06 22:31:01 -0700

    added functioning arrow buttons in documint demo page

--- src/components/DocuMintDemoPage.js
+++ src/components/DocuMintDemoPage.js
@@ -1,6 +1,37 @@
+import { useState } from "react";
+
+const minPageNUmber = 0;
+const maxPageNumber = 2;
+
 const DocuMintDemoPage = () => {
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const handleRightButtonClick = () => {
+        if (pageNumber < maxPageNumber) {
+            setPageNumber(pageNumber + 1);
+        }
+    }
+
+    const handleLeftButtonClick = () => {
+        if (pageNumber > minPageNUmber) {
+            setPageNumber(pageNumber - 1);
+        }
+    }
+
     return (  
-        <div>DocuMint Demo Page</div>
+        <div>
+            <h1>DocuMint Demo</h1>
+            
+            <span className="demo-page-button-left-container">
+                {pageNumber > minPageNUmber && <span className="arrow-button arrow-left " onClick={handleLeftButtonClick}></span>}
+            </span>
+            {pageNumber == 0 && <span className="demo-gif-explanation">First, login to the application using company credentials</span>}
+            {pageNumber == 1 && <span className="demo-gif-explanation">Search for student documents by studentID.</span>}
+            {pageNumber == 2 && <span className="demo-gif-explanation">Filter and sort results to find documents easily.</span>}
+            <span className="demo-page-button-right-container">
+                {pageNumber < maxPageNumber && <span className="arrow-button arrow-right demo-page-button-right" onClick={handleRightButtonClick}></span>}
+            </span>
+        </div>
     );
 }
  
