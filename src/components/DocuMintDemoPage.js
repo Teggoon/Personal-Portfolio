77bdcce9374eb92de9773150090cb5b08b207e1d
Teggoon <daniel.hc.li@gmail.com>
2021-09-06 23:05:22 -0700

    added gif with some layout implemented

--- src/components/DocuMintDemoPage.js
+++ src/components/DocuMintDemoPage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DocuMintLogin from "../gif/DocuMintLogin.gif";
 
 const minPageNUmber = 0;
 const maxPageNumber = 2;
@@ -21,16 +22,20 @@ const DocuMintDemoPage = () => {
     return (  
         <div className="demo-page">
             <h1 className="demo-page-title">DocuMint Demo</h1>
-            
-            <span className="demo-page-button-left-container">
-                {pageNumber > minPageNUmber && <span className="arrow-button arrow-left " onClick={handleLeftButtonClick}></span>}
-            </span>
-            {pageNumber == 0 && <span className="demo-gif-explanation">First, login to the application using company credentials</span>}
-            {pageNumber == 1 && <span className="demo-gif-explanation">Search for student documents by studentID.</span>}
-            {pageNumber == 2 && <span className="demo-gif-explanation">Filter and sort results to find documents easily.</span>}
-            <span className="demo-page-button-right-container">
-                {pageNumber < maxPageNumber && <span className="arrow-button arrow-right demo-page-button-right" onClick={handleRightButtonClick}></span>}
-            </span>
+            <div className="demo-explanation-row-container">
+                <span className="demo-page-button-left-container">
+                    {pageNumber > minPageNUmber && <span className="arrow-button arrow-left " onClick={handleLeftButtonClick}></span>}
+                </span>
+                {pageNumber == 0 && <span className="demo-gif-explanation">First, login to the application using company credentials</span>}
+                {pageNumber == 1 && <span className="demo-gif-explanation">Search for student documents by studentID.</span>}
+                {pageNumber == 2 && <span className="demo-gif-explanation">Filter and sort results to find documents easily.</span>}
+                <span className="demo-page-button-right-container">
+                    {pageNumber < maxPageNumber && <span className="arrow-button arrow-right demo-page-button-right" onClick={handleRightButtonClick}></span>}
+                </span>
+            </div>
+            <div>
+                {pageNumber == 0 && <img className="demo-gif" src={DocuMintLogin}></img>}
+            </div>
         </div>
     );
 }
