9bc303cd44d14cb48b85adf9a27a4f3e4328030d
Teggoon <daniel.hc.li@gmail.com>
2021-09-13 21:23:34 -0700

    modified server configuration

--- src/server/server.js
+++ src/server/server.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../../build')))
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
 
-app.listen(81);
+app.listen(3000);
 
-console.log("app listening on port 81");
+console.log("app listening on port 3000");
