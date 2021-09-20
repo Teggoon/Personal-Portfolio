c26b2ac0ff71dc5bed64a905fa546287509a1d41
Teggoon <daniel.hc.li@gmail.com>
2021-09-19 20:38:08 -0700

    more edits on the background image and animation

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -3,6 +3,7 @@ import sprite1 from "../image/sprite1_clear.png"
 import sprite2 from "../image/sprite2_clear.png"
 import sprite4 from "../image/sprite4_clear.png"
 import sprite5 from "../image/sprite5_clear.png"
+import codingBackground from "../image/coding_background.jpg"
 import sprite6 from "../image/sprite6_clear.png"
 
 const AnimatedBackground = () => {
@@ -12,8 +13,9 @@ const AnimatedBackground = () => {
     <img src={sprite2} id="sprite2" className="animated-sprite"/>
     <img src={sprite1} id="sprite3" className="animated-sprite"/>
     <img src={sprite4} id="sprite4" className="animated-sprite"/>
-    <img src={sprite5} id="sprite5" className="animated-sprite"/>
+    <img src={sprite1} id="sprite5" className="animated-sprite"/>
     <img src={sprite6} id="sprite6" className="animated-sprite"/>
+    <img src={codingBackground} id="coding-background-image"/>
     
     </div>
     );
